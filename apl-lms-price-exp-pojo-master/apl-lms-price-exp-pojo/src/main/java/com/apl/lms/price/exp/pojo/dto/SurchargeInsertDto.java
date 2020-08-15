3b0ab3aec62e17d669e4370d71528b677b23a609
apl0219 <1901080219@aplun.net>
2020-08-15 11:38:01 +0800

    commit

--- apl-lms-price-exp-pojo-master/apl-lms-price-exp-pojo/src/main/java/com/apl/lms/price/exp/pojo/dto/SurchargeInsertDto.java
+++ apl-lms-price-exp-pojo-master/apl-lms-price-exp-pojo/src/main/java/com/apl/lms/price/exp/pojo/dto/SurchargeInsertDto.java
@@ -1,10 +1,14 @@
 package com.apl.lms.price.exp.pojo.dto;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author hjr start
@@ -13,6 +17,10 @@ import javax.validation.constraints.NotBlank;
 @Data
 @ApiModel(value = "附加费插入对象", description = "附加费插入对象")
 public class SurchargeInsertDto {
+
+    @ApiModelProperty(name = "id", value = "附加费Id", hidden = true)
+    private Long id;
+
 
     @ApiModelProperty(name = "chargeName", value = "附加费名称", required = true)
     @NotBlank(message = "附加费名称名称不能为空")
