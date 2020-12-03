e751a619f67a935f6c93f81b8b5710ca487aea2e
arranchen <arran@aplun.net>
2020-12-03 16:25:01 +0800

    up.sh

--- dev-file/sh/up-start.sh
+++ dev-file/sh/up-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#本地项目绝对路径
-projectPath="/Volumes/data/java/apl-lms/apl-lms-price-exp-manage-java"
+#项目绝对路径
+projectPath="/Volumes/data/java/apl-sys"
 
 #远程目标路径
 remoteRootPath="";
@@ -15,11 +15,18 @@ if [ "$hostIndex" -eq "2" ]; then
     remoteRootPath="/home/aapl/java/";
 fi
 
+if [ "$hostIndex" -eq "3" ]; then
+    hostAddr="8.129.236.175"
+    userId="aapl"
+    remoteRootPath="/home/aapl/java/";
+fi
+
 echo "远程主机: $hostAddr"
 
 filePath=$1
 fileName=$2
 remotePath=$3
+port=$4
 
 if [[ $fileName == *"jar" ]];then
       #jar
@@ -27,18 +34,25 @@ if [[ $fileName == *"jar" ]];then
       sshpass -p $password ssh  -o StrictHostKeyChecking=no $userId@$hostAddr "cd $remoteRootPath$remotePath; cp ./$fileName ./bak/; ./startup.sh stop $fileName; "
 
       echo  "上传 $fileName..."
-      sshpass -p $password scp  $projectPath/$filePath$fileName  $userId@$hostAddr:$remoteRootPath$remotePath
       #scp  $projectPath/$filePath$fileName  $userId@$hostAddr:$remoteRootPath$remotePath
+      sshpass -p $password scp  $projectPath/$filePath$fileName  $userId@$hostAddr:$remoteRootPath$remotePath
 
       echo  "远程启动 $fileName..."
-      sshpass -p $password ssh  $userId@$hostAddr "source /etc/profile; cd $remoteRootPath$remotePath; nohup java -jar  $fileName >./logs/$fileName.log 2>&1 &"
+      if [ -z "$port" ]; then
+         sshpass -p $password ssh  $userId@$hostAddr "source /etc/profile; cd $remoteRootPath$remotePath; nohup java -jar  $fileName  --server.port=$port  >./logs/$fileName.log 2>&1 &"
+      else
+         sshpass -p $password ssh  $userId@$hostAddr "source /etc/profile; cd $remoteRootPath$remotePath; nohup java -jar  $fileName  >./logs/$fileName.log 2>&1 &"
+      fi
 
       echo  "上传 $fileName, 并远程启动完成!"
   else
       #非jar
       echo  "上传 $fileName..."
-      sshpass -p $password scp  $projectPath/$filePath$fileName  $userId@$hostAddr:$remoteRootPath$remotePath
       #scp  $projectPath/$filePath$fileName  $userId@$hostAddr:$remoteRootPath$remotePath
+      sshpass -p $password scp  $projectPath/$filePath$fileName  $userId@$hostAddr:$remoteRootPath$remotePath
 
       echo  "上传 $fileName, 完成!"
 fi
+
+
+
